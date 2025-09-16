6f8d38251ad315cfe6d6b61f335a6e6f456b18ce
quanfeng <quanfeng1024@gmail.com>
2025-09-16 22:57:49 +0800

    update: 更新希尔排序的Java表达

--- src/main/java/site/techland/algorithm/basic/Sort.java
+++ src/main/java/site/techland/algorithm/basic/Sort.java
@@ -64,4 +64,20 @@ public class Sort {
             arr[index] = currVal;
         }
     }
+
+    public static void shellSort(int[] arr) {
+        int length = arr.length;
+        int temp;
+        for (int step = length / 2; step >= 1; step /= 2) {
+            for (int i = step; i < length; i++) {
+                temp = arr[i];
+                int j = i - step;
+                while (j >= 0 && arr[j] > temp) {
+                    arr[j + step] = arr[j];
+                    j -= step;
+                }
+                arr[j + step] = temp;
+            }
+        }
+    }
 }
